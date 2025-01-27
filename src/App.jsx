5da05873ad64c32baf50e0e4839a3399e27123c1
Askat <saatbek493@gmail.com>
2025-01-27 23:54:05 +0300

    finished icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Icons } from "./assets";
 
 const App = () => {
-  return <div>SINSAY</div>;
+  return (
+    <div>
+      <Icons.Delete />
+      <Icons.basil_twitter />
+
+      <Icons.iconoir_facebook />
+      <Icons.like />
+      <Icons.likeblack />
+      <Icons.menuburger />
+      <Icons.Poisk />
+
+      <Icons.profile />
+
+      <Icons.Sumka />
+      <Icons.twiter />
+      <Icons.instagramm />
+    </div>
+  );
 };
 
 export default App;
