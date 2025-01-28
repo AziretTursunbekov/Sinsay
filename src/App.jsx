7c873e82a8acc120a102398b5957ff445a88dacf
Askat <saatbek493@gmail.com>
2025-01-28 18:14:07 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,8 @@ import { Icons } from "./assets";
 const App = () => {
   return (
     <div>
-      <Icons.Delete />
-      <Icons.basil_twitter />
-
-      <Icons.iconoir_facebook />
-      <Icons.like />
-      <Icons.likeblack />
-      <Icons.menuburger />
-      <Icons.Poisk />
-
-      <Icons.profile />
-
-      <Icons.Sumka />
-      <Icons.twiter />
-      <Icons.instagramm />
+      <Icons.Delete/>
+      
     </div>
   );
 };
