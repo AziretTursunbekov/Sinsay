a8a2ae241ed4a7531077cd76cda18e3dcc2fdccc
Aziret <azi326064@gmail.com>
2025-01-28 19:20:21 +0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,7 @@ import React from "react";
 import { Icons } from "./assets";
 
 const App = () => {
-  return (
-    <div>
-      <Icons.Delete />
-      <Icons.basil_twitter />
-      <Icons.like />
-      <Icons.likeblack />
-      <Icons.menuburger />
-      <Icons.Poisk />
-      <Icons.profile />
-      <Icons.Sumka />
-      <Icons.twiter />
-      <Icons.instagramm />
-      <Icons.iconoir_facebook />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
