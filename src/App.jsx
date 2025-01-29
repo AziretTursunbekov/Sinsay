ec7051d56e430fc67ed7a1cd1b2ef157f8928923
Rustam <nizomovrustam30@gmail.com>
2025-01-29 18:05:50 +0300

    finished button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Icons } from "./assets";
+import Button from "./components/UI/Button";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      hello
+      <Button variant={"outlined"}>hello</Button>
+      <Button variant={"contained"}>dffds</Button>
+    </div>
+  );
 };
 
 export default App;
