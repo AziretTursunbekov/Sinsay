a53171b02ccd461244a448f3367d6299b9e5bc71
Rustam <nizomovrustam30@gmail.com>
2025-01-30 19:24:28 +0300

    save

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Image from "../../assets/images/image.png";
-import { product } from "../../utils/constants";
+import { product } from "../utils/constants";
 
 const CartList = () => {
   return (
@@ -11,43 +10,45 @@ const CartList = () => {
         <StyledAteg href="#">Product</StyledAteg>
 
         <StyledDivBOOx>
-          <StyledAteg href="#">Price</StyledAteg>
-          <StyledAteg href="#">Quantity</StyledAteg>
-          <StyledAteg href="#">Total</StyledAteg>
+          <div className="firstDiv">
+            <StyledAteg href="#">Price</StyledAteg>
+            <StyledAteg href="#">Quantity</StyledAteg>
+            <StyledAteg href="#">Total</StyledAteg>
+          </div>
         </StyledDivBOOx>
       </StyledLi>
       <StyledUl>
-        {product.map((item, i) => (
-          <StyledDivMap>
-            <StyledDivMap2>
-              <StyledLi key={i}>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    width: "85rem",
-                  }}
-                >
+        {product.map((item) => (
+          <div>
+            <div key={item.id}>
+              <StyledLi>
+                <div className="div">
                   <img src={item.image} alt="" />
                   <StyledP>{item.title}</StyledP>
-                  <SDA>
-                    <StyledSpan1>${item.price}</StyledSpan1>
-                    <StyledContainerDiv>
-                      <StyledCounterButton>-</StyledCounterButton>
-                      <b>1</b>
-                      <StyledCounterButton>+</StyledCounterButton>
-                    </StyledContainerDiv>
-                    <StyledSpan2>${item.price}</StyledSpan2>
-                  </SDA>
+                  <div>
+                    <div>
+                      <StyledPriceDiv>
+                        <StyledSpan1>${item.price}</StyledSpan1>
+                        <StyledContainerDiv>
+                          <StyledCounterButton>-</StyledCounterButton>
+                          <b>1</b>
+                          <StyledCounterButton>+</StyledCounterButton>
+                        </StyledContainerDiv>
+                        <StyledSpan2>${item.price}</StyledSpan2>
+                      </StyledPriceDiv>
+                    </div>
+                  </div>
                 </div>
                 <img src={item.cartina} alt="" />
               </StyledLi>
-            </StyledDivMap2>
-          </StyledDivMap>
+            </div>
+          </div>
         ))}
       </StyledUl>
-      <StyledDIVH1>Subtotal: $86.00</StyledDIVH1>
-      <StyledPP>Tax included. Shipping calculated at checkout.</StyledPP>
+      <StyledSubtotal>
+        <h1>Subtotal: $86.00</h1>
+        <StyledPP>Tax included. Shipping calculated at checkout.</StyledPP>
+      </StyledSubtotal>
       <ButtonContainer>
         <StyledButtOn1> Continue shopping</StyledButtOn1>
         <SyledBUTTON2>Checkout </SyledBUTTON2>
@@ -57,12 +58,22 @@ const CartList = () => {
 };
 
 export default CartList;
-
-const SDA = styled.div`
-  width: 400px;
+const StyledSubtotal = styled.div`
+  width: 100%;
+  height: 150px;
   display: flex;
-
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: end;
+  align-items: end;
+  gap: 8px;
+  padding: 35px 50px;
+`;
+const StyledPriceDiv = styled.div`
+  width: 530px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 90px;
 `;
 
 const StyledCounterButton = styled.button`
@@ -88,22 +99,6 @@ const StyledP = styled.p`
   font-weight: 400;
 `;
 
-const StyledDIVVV = styled.div`
-  display: flex;
-  gap: 120px;
-  align-items: center;
-  /* margin-top: 10px; */
-`;
-const StyledDivMap2 = styled.div`
-  gap: 100px;
-  display: flex;
-  align-items: center;
-`;
-
-const StyledDivMap = styled.div`
-  display: flex;
-  gap: 25px;
-`;
 const StyledUl = styled.ul`
   display: flex;
   flex-direction: column;
@@ -121,10 +116,11 @@ const StyledSpan2 = styled.span`
 `;
 
 const ButtonContainer = styled.div`
-  margin: 30px;
+  width: 100vw;
+
   display: flex;
   justify-content: space-between;
-  width: 37%;
+  padding: 0 50px;
 `;
 
 const StyledButtOn1 = styled.button`
@@ -148,9 +144,6 @@ const SyledBUTTON2 = styled.button`
   border: none;
   padding: 20px 15px;
 `;
-const StyledDIVH1 = styled.h1`
-  margin-top: 50px;
-`;
 
 const StyledPP = styled.p`
   font-size: 20px;
@@ -161,13 +154,14 @@ const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 18px;
 `;
 
 const StyledCart = styled.p`
   align-items: center;
   display: flex;
   justify-content: center;
-
+  padding-bottom: 50px;
   font-size: 81px;
   line-height: 73.2px;
   font-weight: 500px;
@@ -175,24 +169,36 @@ const StyledCart = styled.p`
 
 const StyledAteg = styled.a`
   text-decoration: none;
-  margin-left: 10px;
   color: black;
   font-size: 21px;
   font-weight: 400;
 `;
 
 const StyledLi = styled.li`
-  width: 100rem;
+  width: 90rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 1px solid;
-  padding: 10px 120px 25px 120px;
+
+  padding: 10px 60px 25px 60px;
+  .div {
+    width: 300rem;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 90px;
+  }
 `;
 
 const StyledDivBOOx = styled.div`
-  gap: 100px;
-  padding: 5px;
+  width: 560px;
   display: flex;
   justify-content: start;
+  .firstDiv {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 90px;
+  }
 `;
