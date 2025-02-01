a2aabc48fe3fe26ac37c418cadb7645016850042
Sezimay <zolcumamatovasezimaj@gmail.com>
2025-02-01 18:14:47 +0300

    changed cartlist

--- src/components/Whishlist.jsx
+++ src/components/Whishlist.jsx
@@ -1,66 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import { product } from "../utils/constatnts";
 import { Icons } from "../assets";
-import { carta } from "../utils/constatnts";
 
-export const WishList = () => {
+export default function WishList() {
   return (
     <StyledWishList>
       <StyledTitle>Wish list</StyledTitle>
       <StyledSection>
-        <TopRow>
-          {carta.slice(0, 3).map((item) => (
-            <StyledArticle key={item.id}>
-              <StyledImage src={item.image} alt="img" />
-              <Description>{item.description}</Description>
+        {product.map((item) => (
+          <article key={item.id}>
+            <img src={item.image} alt="img" />
+            <Description>{item.description}</Description>
 
-              <PriceContainer>
-                <Icons.likeblack />
-                <h3>{item.price}</h3>
-              </PriceContainer>
+            <PriceContainer>
+              <h3>{item.price}</h3>
+              <Icons.likeblack />
+            </PriceContainer>
 
-              <Button>Add to cart</Button>
+            <Button>Add to cart</Button>
 
-              <DeleteContainer>
-                <Icons.Delete />
-                <Delete>Delete</Delete>
-              </DeleteContainer>
-            </StyledArticle>
-          ))}
-        </TopRow>
-        <BottomRow>
-          {carta.slice(3, 6).map((item) => (
-            <StyledArticle key={item.id}>
-              <StyledImage src={item.image} alt="img" />
-              <Description>{item.description}</Description>
-
-              <PriceContainer>
-                <Icons.likeblack />
-                <h3>{item.price}</h3>
-              </PriceContainer>
-
-              <Button>Add to cart</Button>
-
-              <DeleteContainer>
-                <Icons.Delete />
-                <Delete>Delete</Delete>
-              </DeleteContainer>
-            </StyledArticle>
-          ))}
-        </BottomRow>
+            <DeleteContainer>
+              <Icons.Delete />
+              <Delete>Delete</Delete>
+            </DeleteContainer>
+          </article>
+        ))}
       </StyledSection>
     </StyledWishList>
   );
-};
+}
 
 const StyledWishList = styled.div`
-  font-family: "Roboto", sans-serif;
-  width: 1250px;
+  width: 100%;
+  height: 1500px;
+  gap: 60px;
   padding: 40px 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: auto;
 `;
 
 const StyledTitle = styled.h1`
@@ -70,34 +48,14 @@ const StyledTitle = styled.h1`
 `;
 
 const StyledSection = styled.section`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-const TopRow = styled.div`
+  width: 90rem;
+  height: 666px;
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
   gap: 20px;
-`;
-
-const BottomRow = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-`;
-
-const StyledArticle = styled.article`
-  text-align: center;
-  transition: transform 0.2s;
-`;
-
-const StyledImage = styled.img`
-  transition: transform 0.2s, filter 0.2s;
-
-  &:hover {
-    transform: translateY(-5px);
+  article {
+    width: 413px;
   }
 `;
 
@@ -112,21 +70,17 @@ const Description = styled.p`
   font-size: x-large;
   padding: 10px;
 `;
-
 const Button = styled.button`
-  background-color: black;
+  background: black;
+  color: white;
   border: none;
-  border-radius: 10px;
-  color: #ffffff;
   cursor: pointer;
-  width: 100%;
+  width: 413px;
   height: 69px;
   font-size: larger;
   font-weight: bold;
-  transition: transform 0.2s;
-
   &:hover {
-    transform: translateY(-3px);
+    background: gray;
   }
 `;
 
@@ -135,17 +89,13 @@ const DeleteContainer = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 20px;
+  cursor: pointer;
 `;
-
 const Delete = styled.button`
   width: 94px;
   height: 25px;
   font-size: larger;
   background-color: white;
   border: none;
-  transition: transform 0.2s;
-
-  &:hover {
-    transform: translateY(-3px);
-  }
+  cursor: pointer;
 `;
