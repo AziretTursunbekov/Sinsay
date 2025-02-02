140313540e7257b9f8c8351b18f4fc88fa61ba9a
Askat <saatbek493@gmail.com>
2025-02-02 21:23:50 +0300

    save

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,26 +1,3 @@
-// import { createContext, useState } from "react";
-// import { PRODUCTS } from "../utils/constatnts";
-// export const ProductContext = createContext();
-
-// export const ProductProvider = ({ children }) => {
-//   const [products, setProducts] = useState(PRODUCTS);
-//   const favoriteHandler = (id) => {
-//     const currentMap = products.map((item) => {
-//       if (item.id === id) {
-//         return { ...item, favorite: !item.favorite };
-//       }
-//       return item;
-//     });
-//     setProducts(currentMap);
-//   };
-
-//   return (
-//     <ProductContext.Provider value={{ favoriteHandler }}>
-//       {children}
-//     </ProductContext.Provider>
-//   );
-// };
-
 import { createContext, useState } from "react";
 
 export const ProductContext = createContext();
